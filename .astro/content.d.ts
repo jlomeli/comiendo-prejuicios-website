d2dda497ca9a67fd81cba137910ff44cd6fac323
Jesus Lomeli <lomelister@gmail.com>
2025-06-27 16:23:37 -0600

    Configures content collections for services

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -147,10 +147,20 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		
+		"services": Record<string, {
+  id: string;
+  render(): Render[".md"];
+  slug: string;
+  body: string;
+  collection: "services";
+  data: InferEntrySchema<"services">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content.config.mjs");
+	export type ContentConfig = typeof import("../src/content/config.js");
 }
